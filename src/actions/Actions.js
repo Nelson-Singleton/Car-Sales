20e11ddacf417c9943d564427041a6a3728c7819
nelson-singleton <nsingleton@ymail.com>
2020-09-08 22:51:05 -0500

    Connected AdditionalFeatures to redux. Created action object to add a feature

---
+++ src/actions/Actions.js
@@ -0,0 +1,8 @@
+//action creator: Function that creates actions
+//a function that returns an action object
+
+//add feature on click
+export const ADD_FEATURE = "ADD_FEATURE";
+export const addFeature = (addedFeature) => {
+    return {type: ADD_FEATURE, payload: addedFeature}
+}
